2c090c2f1ac3c97a058b4e338b0c2325e9274c02
sasukevg <nikitatarnik@gmail.com>
2023-10-02 17:34:24 +0300

    upd

--- main.py
+++ main.py
@@ -22,26 +22,23 @@ combo = []
 for j in range(M):
     Mass_Rand = input_array()
     combo.append(Mass_Rand)
+    arrays_padded = [arr + [0] * (len(Mass_X) - len(arr)) for arr in combo]
+    Mass_Z = [sum(x) for x in zip(*arrays_padded)]
 
-# Сумма элементов массивов
+    print(Mass_Z)
 
-arrays_padded = [arr + [0] * (len(Mass_X)  - len(arr)) for arr in combo]
-Mass_Z = [sum(x) for x in zip(*arrays_padded)]
+    # Поиск Массива Y
 
-print(Mass_Z)
+    Mass_Y = np.zeros(len(Mass_X))
 
-# Поиск Массива Y
+    max_Mass_Z = max(Mass_Z)
+    id_of_max = Mass_Z.index(max_Mass_Z)
+    k = max_Mass_Z / Mass_X[id_of_max]
 
-Mass_Y = np.zeros(len(Mass_X))
+    for i in range(len(Mass_X)):
+        if Mass_Z[i] / Mass_X[i] != k:
+            Mass_Y[i] = k * Mass_X[i] - Mass_Z[i]
+        else:
+            Mass_Y[i] = 0
 
-max_Mass_Z = max(Mass_Z)
-id_of_max = Mass_Z.index(max_Mass_Z)
-k = max_Mass_Z / Mass_X[id_of_max]
-
-for i in range(len(Mass_X)):
-    if Mass_Z[i] / Mass_X[i] != k:
-        Mass_Y[i] = k * Mass_X[i] - Mass_Z[i]
-    else:
-        Mass_Y[i] = 0
-
-print(Mass_Y)
+    print(Mass_Y)
